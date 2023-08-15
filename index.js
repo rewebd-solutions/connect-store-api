60cad9c9d185898cf78a877acea84e5fa9ecd427
rewebd-solutions <127234723+rewebd-solutions@users.noreply.github.com>
2023-08-15 20:09:49 +0530

    Implemented CORS

--- index.js
+++ index.js
@@ -1,17 +1,19 @@
 import Express from 'express'
+import cors from 'cors';
 
 const app = Express()
 
+app.use(cors())
 app.use(Express.json())
 
 app.post("/connect-shopify", async (req, res) => {
     const reqBody = req.body;
-
+    // console.log(reqBody);
     const SHOPIFY_API_KEY = 'cd05b26a775ef288edb38cc689393e9c';
     const SHOPIFY_SECRET_KEY = '2ef5f09f546e58780a58c2a00bab8428';
     const SHOPIFY_ACCESS_TOKEN = reqBody.access_token;
     const SHOPIFY_SHOP_URL = reqBody.store_url
-
+    // console.log(SHOPIFY_ACCESS_TOKEN + SHOPIFY_SHOP_URL)
     const shopifyEndpoint = `https://${SHOPIFY_SHOP_URL}.myshopify.com/admin/api/2023-07/orders.json?status=open&fields=created_at,id,name,total-price,contact-email`
 
     const fetchReq = await fetch(shopifyEndpoint, {

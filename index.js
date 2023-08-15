c93e568996c36710161c1dde61a2a21423031d3e
rewebd-solutions <127234723+rewebd-solutions@users.noreply.github.com>
2023-08-15 19:25:00 +0530

    Added connect option

--- index.js
+++ index.js
@@ -4,11 +4,31 @@ const app = Express()
 
 app.use(Express.json())
 
-app.post("/connect-shopify", (req, res) => {
-    console.log(req.body);
-    res.status(200).send("OK")
+app.post("/connect-shopify", async (req, res) => {
+    const reqBody = req.body;
+
+    const SHOPIFY_API_KEY = 'cd05b26a775ef288edb38cc689393e9c';
+    const SHOPIFY_SECRET_KEY = '2ef5f09f546e58780a58c2a00bab8428';
+    const SHOPIFY_ACCESS_TOKEN = reqBody.access_token;
+    const SHOPIFY_SHOP_URL = reqBody.store_url
+
+    const shopifyEndpoint = `https://${SHOPIFY_SHOP_URL}.myshopify.com/admin/api/2023-07/orders.json?status=open&fields=created_at,id,name,total-price,contact-email`
+
+    const fetchReq = await fetch(shopifyEndpoint, {
+        headers: {
+            'X-Shopify-Access-Token': SHOPIFY_ACCESS_TOKEN
+        }
+    })
+    const fetchData = await fetchReq.json();
+
+    res.status(200).json(fetchData);
 })
 
 app.listen(8080, () => {
     console.log("App is running")
 })
+
+// {
+//     "access_token": "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",
+//     "store_url": "testing-another-shop-data"
+//   }
